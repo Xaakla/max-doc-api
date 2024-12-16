eb274ba82a72a470ec632bb2a50eb8b603668281
xaakla <diego.rb0307@gmail.com>
2024-12-16 17:05:54 -0300

    first commit

---
+++ src/main/java/com/app/maxdocapi/MaxDocApiApplication.java
@@ -0,0 +1,13 @@
+package com.app.maxdocapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxDocApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaxDocApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/maxdocapi/MaxDocApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.maxdocapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxDocApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
